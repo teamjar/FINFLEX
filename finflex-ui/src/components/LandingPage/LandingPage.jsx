4b30f7d5184bc5f0348f494696bce06d50dc9fab
Roy Delgado <roy.delgado63@myhunter.cuny.edu>
2023-07-17 15:45:25 -0400

    landing page

--- finflex-ui/src/components/LandingPage/LandingPage.jsx
+++ finflex-ui/src/components/LandingPage/LandingPage.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import "./LandingPage.css"
+
 const LandingPage = () => { 
     return (
         <div className = 'landing-page'>
             <div className ='container'>
                 <div className = 'info'> 
-                    <h1>FinFlex</h1>
-                    <h2> <span>Lorem ipsum dolor sit amet, consec tetur adipisicing elit. </span></h2>
+                    <h1 style={{fontSize:"150px"}}>FinFlex</h1>
+                    <h2> <span>Cute tagline </span></h2>
                     <p> Lorem ipsum dolor sit amet, consec tetur adipisicing elit.
                         Architecto iure fuga deleniti sit! Cum doloribus, nesciunt
                         laboriosam eos praesentium veritatis</p>
