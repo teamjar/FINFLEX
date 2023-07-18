338e74b193cd964c695454cb46aeead53365a81e
Anaya Bussey <anayabussey@gmail.com>
2023-07-18 10:50:18 -0400

    Commit

--- finflex-ui/src/components/App/App.jsx
+++ finflex-ui/src/components/App/App.jsx
@@ -1,6 +1,6 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import LandingPage from '../LandingPage/LandingPage'
-import LoginPage from '../LoginPage/LoginPage'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LandingPage from '../LandingPage/LandingPage';
+import LoginPage from '../LoginPage/LoginPage';
 
 function App() {
   return (
