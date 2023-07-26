60bbcd4700e32865601ad70110c057f9a6e25bb5
astroboyprograms <103838119+astroboyprograms@users.noreply.github.com>
2023-07-26 15:23:42 -0400

    Buy Stock functionality

---
+++ finflex-ui/src/components/BuyStockPage/BuyStockPage.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import './BuyStockPage.css'
+import BuyStock from '../BuyStock/BuyStock'
+
+function BuyStockPage() {
+  return (
+    <div className='bella-hadid'>
+        <BuyStock />
+        </div>
+  )
+}
+
+export default BuyStockPage
+
+
+// import LoginForm from '../LoginForm/LoginForm.jsx';
+// import { Link } from 'react-router-dom'; 
+// import './LoginPage.css'
+
+// const LoginPage = () => {
+//     return (
+//         <div className='font'>
+//             <LoginForm />
+
+//         </div>
+//     );
+// }
+
+// export default LoginPage;
+
+
+
