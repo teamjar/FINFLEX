df2618b631b98b02e61d9bd3d0da074d5450dbb0
astroboyprograms <103838119+astroboyprograms@users.noreply.github.com>
2023-07-27 18:08:49 -0400

    BuyStock feature

--- finflex-ui/src/components/BuyStock/BuyStock.jsx
+++ finflex-ui/src/components/BuyStock/BuyStock.jsx
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import './BuyStock.css';
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import { fetchStockDetails, fetchQuote } from '../api/stock-api';
 import axios from 'axios';
 import { remoteHostURL } from '../../apiClient'
+import Swal from 'sweetalert2';
 
 function BuyStock() {
     const { symbol } = useParams();
+    const navigate = useNavigate();
     const [quantity, setQuantity] = useState(0);
     const [investment, setInvestment] = useState(0);
     const [stockPrice, setStockPrice] = useState(0);
@@ -47,10 +49,26 @@ function BuyStock() {
                 quantity: quantity,
                 change: change
             })
-            const newArray = [...array, res.data.user];
-            setArray(newArray);
+            if (res.data && res.data.user) {
+                const newArray = [...array, res.data.user];
+                setArray(newArray);
+                Swal.fire({
+                    title: 'Purchase Complete!',
+                    text: `You purchased ${quantity} shares.`,
+                    icon: 'success'
+                }).then(() => {
+                    navigate('/stocks'); // Navigate to stocks route after closing the Swal alert
+                });
+            } else {
+                console.error('Data is not available in the response');
+            }
         } catch (error) {
-            console.log(error)
+            console.error(error);
+            Swal.fire({
+                title: 'Error!',
+                text: 'There was an error processing your purchase.',
+                icon: 'error'
+            });
         }
     }
 
