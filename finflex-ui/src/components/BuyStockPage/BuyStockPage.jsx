8473add0622fe17e88c4be9c422831794553c2d6
astroboyprograms <103838119+astroboyprograms@users.noreply.github.com>
2023-07-27 13:23:36 -0400

    StockDashbaord 3.0

--- finflex-ui/src/components/BuyStockPage/BuyStockPage.jsx
+++ finflex-ui/src/components/BuyStockPage/BuyStockPage.jsx
@@ -13,20 +13,6 @@ function BuyStockPage() {
 export default BuyStockPage
 
 
-// import LoginForm from '../LoginForm/LoginForm.jsx';
-// import { Link } from 'react-router-dom'; 
-// import './LoginPage.css'
-
-// const LoginPage = () => {
-//     return (
-//         <div className='font'>
-//             <LoginForm />
-
-//         </div>
-//     );
-// }
-
-// export default LoginPage;
 
 
 
