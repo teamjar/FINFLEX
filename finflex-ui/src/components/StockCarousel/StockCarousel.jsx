8bf1fd265bbf2d92a72400be412f5c97cf0639b3
astroboyprograms <103838119+astroboyprograms@users.noreply.github.com>
2023-07-31 12:08:12 -0400

    Real-time Prices and Changes

--- finflex-ui/src/components/StockCarousel/StockCarousel.jsx
+++ finflex-ui/src/components/StockCarousel/StockCarousel.jsx
@@ -1,52 +1,67 @@
-import React from 'react'
 import './StockCarousel.css'
 import { Link } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import axios from 'axios'
 import { remoteHostURL } from '../../apiClient';
+import { fetchQuote } from '../api/stock-api';
 
 function StockCarousel() {
-  const [stocks, setStocks] = useState([]); 
-  const colors = ['color-1', 'color-2', 'color-3', 'color-4'];
-  
+ const [stocks, setStocks] = useState([]);
+ const colors = ['color-1', 'color-2', 'color-3', 'color-4'];
   const scroll = (scrollOffset) => {
-    document.querySelector('.carousel').scrollLeft += scrollOffset;
+   document.querySelector('.carousel').scrollLeft += scrollOffset;
+ };
+
+ useEffect(() => {
+  const fetchStocks = async () => {
+    try {
+      const userId = localStorage.getItem('userId');
+      const res = await axios.get(`${remoteHostURL}/stocks/${userId}`);
+      if (res?.data?.database) {
+        const updatedStocks = await Promise.all(
+          res.data.database.map(async (stock) => {
+            const quote = await fetchQuote(stock.ticker);
+            return {
+              ...stock,
+              stockprice: quote.c,
+              change: quote.dp,
+            };
+          }),
+        );
+        setStocks(updatedStocks);
+      }
+    } catch (err) {
+      console.log(err);
+    }
   };
 
-  useEffect(() => {
-    const fetchStocks = async () => {
-      try {
-        const userId = localStorage.getItem('userId')
-        const res = await axios.get(`${remoteHostURL}/stocks/${userId}`);
-        if (res?.data?.database) {
-          setStocks(res.data.database);
-        }
-      } catch (err) {
-        console.log(err);
-      }
-    };
+  fetchStocks();
 
-    fetchStocks();
-  }, [])
+  const interval = setInterval(fetchStocks, 30000); 
 
-  return (
-    <div className="carousel-container">
-      <button className="scroll-button left" onClick={() => scroll(-200)}>←</button>
-      <div className="carousel">
-      {stocks.map((stock, index) => (
-        <Link to={`/stock/${stock.id}`} key={stock.id}> 
-          <div key={stock.id} className={`stock-card ${colors[index % 4]}`}>
-            <h4>{stock.companyname}</h4>
-            <p>Price: ${stock.stockprice}</p>
-            <p>Change: {stock.change}</p>
-          </div>
-        </Link>
-      
-        ))}
-      </div>
-      <button className="scroll-button right" onClick={() => scroll(200)}>→</button>
-    </div>
-  );
+  return () => clearInterval(interval);
+}, []);
+
+
+ return (
+   <div className="carousel-container">
+     <button className="scroll-button left" onClick={() => scroll(-200)}>←</button>
+     <div className="carousel">
+     {stocks.map((stock, index) => (
+       <Link to={`/stock-details/${stock.ticker}`} key={stock.ticker}>
+         <div key={stock.id} className={`stock-card ${colors[index % 4]}`}>
+           <h4 style={{color:"white", textAlign:"center", fontWeight:"bolder"}}>{stock.companyname}</h4>
+           <p style={{fontWeight:"bolder"}}>Price: ${stock.stockprice}</p>
+           <p className={stock.change > 0 ? "green" : "red"} style={{fontWeight:"bolder"}}>Change: {stock.change} {stock.change > 0 ? '↑' : '↓'}</p>
+           <img src={stock.logo} alt={`${stock.companyname} logo`} className="stock-logo" />
+         </div>
+       </Link>
+    
+       ))}
+     </div>
+     <button className="scroll-button right" onClick={() => scroll(200)}>→</button>
+   </div>
+ );
 }
 
 export default StockCarousel;
