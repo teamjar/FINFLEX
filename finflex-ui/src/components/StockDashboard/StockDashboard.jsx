e22da322d0d301da5d8a23fccb114ecb0164cff4
astroboyprograms <103838119+astroboyprograms@users.noreply.github.com>
2023-08-01 11:48:24 -0400

    NewsFeed

--- finflex-ui/src/components/StockDashboard/StockDashboard.jsx
+++ finflex-ui/src/components/StockDashboard/StockDashboard.jsx
@@ -1,28 +1,35 @@
+import React from 'react';
 import { StockProvider } from '../../Context/StockContext'; 
 import StockSide from '../StockSide/StockSide';
 import Navbar from '../Navbar/Navbar';
 import './StockDashbord.css';
 import StockSearch from '../StockSearch/StockSearch';
 import StockCarousel from '../StockCarousel/StockCarousel';
+import axios from 'axios';
+import Card from '../Card/Card';
+
+
+
 
 function StockDashboard() {
-  return (
-  <div>
-    <StockSide />
-  <div className='plz'>
-    {/* <Navbar /> */}
-    <StockProvider>
-      <div className='stock-dashboard'>
-      <div className="Navbar"><Navbar/></div>
-      <div className="StockSearch"> <StockSearch/></div>
 
-      <div className="StockCarousel"> <StockCarousel/></div>
-      </div>
-    </StockProvider>
-  </div>
-</div>
-  );
+
+  return (
+      <div>
+       <StockSide />
+       <div className='plz'>
+         <StockProvider>
+           <div className='stock-dashboard'>
+             <div className="Navbar"><Navbar/></div>
+             <div className="StockSearch"> <StockSearch/></div>
+             <div className="StockCarousel"> <StockCarousel/></div>
+           </div>
+         </StockProvider>
+       </div>
+     </div>
+
+    
+   );
 }
 
 export default StockDashboard;
-
